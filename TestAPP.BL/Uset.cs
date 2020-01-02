97b433806c590f5d789f650e78bdf998f6d213ab
Алексей Тарасов <leha-1499@inbox.ru>
2020-01-02 21:47:42 +0300

    Создал тестовую модель Uset

---
+++ TestAPP.BL/Uset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAPP.BL
+{
+    public class Uset
+    {
+        public string Name { get; set; }
+        public Uset():this("123")
+        {
+
+        }
+        public Uset(string name)
+        {
+            Name = name;
+        }
+    }
+}
--- TestAPP/Program.cs
+++ TestAPP/Program.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestAPP.BL;
 
 namespace TestAPP
 {
 	class Program
 	{
-		static string a = "123";
+		static string a = "AASDDDDD";
 		static void Main(string[] args)
 		{
-
+			var Uset = new Uset(a);
 		}
 	}
 }
